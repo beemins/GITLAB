0cd16fce5ad1743ee4c1671d68e4cedd37dd9f0e
beemins <boom_reban@hotmail.com>
2015-04-25 13:02:46 +0700

    Add all files

---
+++ Program1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+void main()
+{
+   int value = 1;
+ 
+   while(value<=5)
+   {
+      printf("Value is %d\n", value);
+      value++;
+   }
+}
---
+++ Program2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+void main()
+{
+   float input;
+ 
+   printf("Enter a number\n");
+   scanf("%f",&input);
+ 
+   printf("Number is %f\n", input);
+}
